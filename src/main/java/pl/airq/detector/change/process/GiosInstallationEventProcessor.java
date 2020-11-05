243ad5e796d7ae110c27ca2c889770f6b8c8969c
Adam Ruminski <cssrumi@gmail.com>
2020-11-05 19:34:41 +0100

    Outgoing topic added to metadata

--- src/main/java/pl/airq/detector/change/process/GiosInstallationEventProcessor.java
+++ src/main/java/pl/airq/detector/change/process/GiosInstallationEventProcessor.java
@@ -5,17 +5,16 @@ import io.smallrye.mutiny.Uni;
 import io.smallrye.reactive.messaging.kafka.OutgoingKafkaRecordMetadata;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.eclipse.microprofile.reactive.messaging.Message;
+import org.eclipse.microprofile.reactive.messaging.OnOverflow;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import pl.airq.common.process.AppEventBus;
 import pl.airq.common.process.EventParser;
-import pl.airq.common.process.MutinyUtils;
 import pl.airq.common.process.ctx.gios.installation.GiosInstallationEventPayload;
 import pl.airq.common.process.event.AirqEvent;
-import pl.airq.common.store.key.TSFKey;
 import pl.airq.common.store.key.TSKey;
 import pl.airq.detector.change.domain.gios.GiosInstallation;
 import pl.airq.detector.change.domain.gios.GiosInstallationEventType;
@@ -23,39 +22,43 @@ import pl.airq.detector.change.domain.gios.GiosInstallationEventType;
 import static pl.airq.detector.change.process.TopicConstants.GIOS_INSTALLATION_TOPIC;
 
 @ApplicationScoped
-class GiosInstallationEventConsumer {
+class GiosInstallationEventProcessor {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(GiosInstallationEventConsumer.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(GiosInstallationEventProcessor.class);
 
-    private final AppEventBus bus;
-    private final EventParser parser;
     private final Emitter<String> installationEmitter;
+    private final EventParser parser;
+    private final String topic;
 
     @Inject
-    GiosInstallationEventConsumer(AppEventBus bus, EventParser parser,
-                                  @Channel("gios-installation") Emitter<String> installationEmitter) {
-        this.bus = bus;
+    GiosInstallationEventProcessor(@OnOverflow(value = OnOverflow.Strategy.BUFFER, bufferSize = 100)
+                                   @Channel("gios-installation") Emitter<String> installationEmitter,
+                                   @ConfigProperty(name = "mp.messaging.outgoing.data-enriched.topic") String topic,
+                                   EventParser parser) {
+
         this.parser = parser;
         this.installationEmitter = installationEmitter;
+        this.topic = topic;
     }
 
     @ConsumeEvent(GIOS_INSTALLATION_TOPIC)
     Uni<Void> consumeEvent(GiosInstallation event) {
         return Uni.createFrom().voidItem()
-                  .invoke(ignore -> {
+                  .invoke(() -> {
                       final GiosInstallationEventType type = event.payload.type;
                       final TSKey key = TSKey.from(event.payload.installation);
                       final AirqEvent<GiosInstallationEventPayload> airqEvent = type.intoAirqEvent(event);
                       final String rawEvent = parser.parse(airqEvent);
-                      OutgoingKafkaRecordMetadata<String> metadata = OutgoingKafkaRecordMetadata
-                              .<String>builder()
-                              .withKey(key.value())
+                      OutgoingKafkaRecordMetadata<TSKey> metadata = OutgoingKafkaRecordMetadata
+                              .<TSKey>builder()
+                              .withTopic(topic)
+                              .withKey(key)
                               .build();
                       final Message<String> message = Message.of(rawEvent)
                                                              .addMetadata(metadata);
                       installationEmitter.send(message);
                       LOGGER.info("GiosMeasurementEvent type: {} send.", type);
                   })
-                  .flatMap(MutinyUtils::ignoreUniResult);
+                  .onItem().ignore().andContinueWithNull();
     }
 }
